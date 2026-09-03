318dbff93c75a058c08586b4da433c97c57059a3
pewpew695 <ashpd02@gmail.com>
2026-09-03 07:10:37 +0530

    - Create a Login Page with Buttons/InputBox - Give state variables to connect input box whenever call is made - Install axios (Used to make API calls instead of using fetch) - In backend code, install cors & use as a middleware in the app with configurations: origins, credentials:true - Whenever an API call is being done with axios pass =>{withCredentials:true}

--- README.md
+++ README.md
@@ -9,3 +9,17 @@
 - Create a BrowserRouter > Routes > Route=/ & Body > RouteChildren
 - Create an Outlet in Body Component
 - Create a Footer.jsx at the bottom
+- Create a Login Page with Buttons/InputBox
+- Give state variables to connect input box whenever call is made
+- Install axios (Used to make API calls instead of using fetch)
+- In backend code, install cors & use as a middleware in the app with configurations: origins, credentials:true
+- Whenever an API call is being done with axios pass =>{withCredentials:true}
+
+Outline of WebPage:
+\_Body
+\_NavBar
+**Route=/ =>Feed
+**Route=/login =>Login
+**Route=/connections =>Connections
+**Route=/profile =>Profile
+\_Footer
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,7 +1,71 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [emailId, setEmailID] = useState("Hello12@gmail.com");
+  const [password, setPassword] = useState("Hello12@gmail.com");
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(
+        "http://localhost:3000/login",
+        {
+          emailId,
+          password,
+        },
+        { withCredentials: true },
+      );
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div className="flex justify-center my-10">
+      <div className="card bg-base-300 w-96 shadow-sm ">
+        <div className="card-body">
+          <h2 className="card-title">Login: {emailId}</h2>
+          <div className="">
+            <div>
+              <fieldset className="fieldset flex">
+                <label className="label w-1/5" htmlFor="email">
+                  Email Id
+                </label>
+                <input
+                  type="text"
+                  id="email"
+                  className="input"
+                  placeholder="Email"
+                  value={emailId}
+                  onChange={(e) => setEmailID(e.target.value)}
+                />
+              </fieldset>
+            </div>
+            <div>
+              <fieldset className="fieldset flex">
+                <label className="label w-1/5" htmlFor="password">
+                  Password
+                </label>
+                <input
+                  type="text"
+                  id="password"
+                  className="input"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </fieldset>
+            </div>
+          </div>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary" onClick={handleLogin}>
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
