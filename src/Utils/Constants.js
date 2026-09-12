ea1180bea98d61cf736c6c435d74fca439cba80b
pewpew695 <ashpd02@gmail.com>
2026-09-12 09:44:04 +0530

    updated BASE_URL to /api as did proxy pass as nginx config

--- src/Utils/Constants.js
+++ src/Utils/Constants.js
@@ -1,5 +1,5 @@
 import profileImage from "../Assets/DefaultUserProfileAvatar.png";
 
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "/api";
 
 export const DEFAULT_PHOTO_URL = profileImage;
